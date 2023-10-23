b65bec83ad5e26ed5b15f15b5bbf2e9b0294535a
rui-mo <rui.mo@intel.com>
2023-10-23 08:56:05 +0800

    Issues calling reclaimer / arbitrator APIs in single-thread execution (5790)

--- velox/common/memory/tests/MemoryManagerTest.cpp
+++ velox/common/memory/tests/MemoryManagerTest.cpp
@@ -122,7 +122,7 @@ class FakeTestArbitrator : public MemoryArbitrator {
     VELOX_NYI();
   }
 
-  void releaseMemory(MemoryPool* pool) override {
+  uint64_t releaseMemory(MemoryPool* pool, uint64_t bytes) override {
     VELOX_NYI();
   }
 
@@ -206,9 +206,6 @@ TEST_F(MemoryManagerTest, addPoolWithArbitrator) {
   options.allocator = allocator.get();
   options.capacity = kCapacity;
   options.arbitratorKind = arbitratorKind_;
-  // The arbitrator capacity will be overridden by the memory manager's
-  // capacity.
-  options.capacity = options.capacity;
   const uint64_t initialPoolCapacity = options.capacity / 32;
   options.memoryPoolInitCapacity = initialPoolCapacity;
   MemoryManager manager{options};
